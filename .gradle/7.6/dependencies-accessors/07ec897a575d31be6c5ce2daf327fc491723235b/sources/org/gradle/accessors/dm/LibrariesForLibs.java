a8afafc75f08b85d188805fc3513c1ca0808ee5b
Ikhun Um <ih.pert@gmail.com>
2022-12-10 20:42:37 +0900

    Add a reproduciable example

---
+++ .gradle/7.6/dependencies-accessors/07ec897a575d31be6c5ce2daf327fc491723235b/sources/org/gradle/accessors/dm/LibrariesForLibs.java
@@ -0,0 +1,95 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.MinimalExternalModuleDependency;
+import org.gradle.plugin.use.PluginDependency;
+import org.gradle.api.artifacts.ExternalModuleDependencyBundle;
+import org.gradle.api.artifacts.MutableVersionConstraint;
+import org.gradle.api.provider.Provider;
+import org.gradle.api.model.ObjectFactory;
+import org.gradle.api.provider.ProviderFactory;
+import org.gradle.api.internal.catalog.AbstractExternalDependencyFactory;
+import org.gradle.api.internal.catalog.DefaultVersionCatalog;
+import java.util.Map;
+import javax.inject.Inject;
+
+/**
+ * A catalog of dependencies accessible via the `libs` extension.
+*/
+@NonNullApi
+public class LibrariesForLibs extends AbstractExternalDependencyFactory {
+
+    private final AbstractExternalDependencyFactory owner = this;
+    private final NettyLibraryAccessors laccForNettyLibraryAccessors = new NettyLibraryAccessors(owner);
+    private final VersionAccessors vaccForVersionAccessors = new VersionAccessors(providers, config);
+    private final BundleAccessors baccForBundleAccessors = new BundleAccessors(objects, providers, config);
+    private final PluginAccessors paccForPluginAccessors = new PluginAccessors(providers, config);
+
+    @Inject
+    public LibrariesForLibs(DefaultVersionCatalog config, ProviderFactory providers, ObjectFactory objects) {
+        super(config, providers, objects);
+    }
+
+    /**
+     * Returns the group of libraries at netty
+     */
+    public NettyLibraryAccessors getNetty() { return laccForNettyLibraryAccessors; }
+
+    /**
+     * Returns the group of versions at versions
+     */
+    public VersionAccessors getVersions() { return vaccForVersionAccessors; }
+
+    /**
+     * Returns the group of bundles at bundles
+     */
+    public BundleAccessors getBundles() { return baccForBundleAccessors; }
+
+    /**
+     * Returns the group of plugins at plugins
+     */
+    public PluginAccessors getPlugins() { return paccForPluginAccessors; }
+
+    public static class NettyLibraryAccessors extends SubDependencyFactory {
+        private final NettyTcnativeLibraryAccessors laccForNettyTcnativeLibraryAccessors = new NettyTcnativeLibraryAccessors(owner);
+
+        public NettyLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+        /**
+         * Returns the group of libraries at netty.tcnative
+         */
+        public NettyTcnativeLibraryAccessors getTcnative() { return laccForNettyTcnativeLibraryAccessors; }
+
+    }
+
+    public static class NettyTcnativeLibraryAccessors extends SubDependencyFactory {
+
+        public NettyTcnativeLibraryAccessors(AbstractExternalDependencyFactory owner) { super(owner); }
+
+            /**
+             * Creates a dependency provider for boringssl (io.netty:netty-tcnative-boringssl-static)
+             * This dependency was declared in catalog libs.versions.toml
+             */
+            public Provider<MinimalExternalModuleDependency> getBoringssl() { return create("netty.tcnative.boringssl"); }
+
+    }
+
+    public static class VersionAccessors extends VersionFactory  {
+
+        public VersionAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+    public static class BundleAccessors extends BundleFactory {
+
+        public BundleAccessors(ObjectFactory objects, ProviderFactory providers, DefaultVersionCatalog config) { super(objects, providers, config); }
+
+    }
+
+    public static class PluginAccessors extends PluginFactory {
+
+        public PluginAccessors(ProviderFactory providers, DefaultVersionCatalog config) { super(providers, config); }
+
+    }
+
+}
